65563d18bec659ef3bd0085b9f75b58d203a9069
bezrukov <bezrukov-yra@mail.ru>
2021-01-08 23:25:01 +0300

    remove cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/clinic'),
-    CatsModule,
     UsersModule,
   ],
 })
